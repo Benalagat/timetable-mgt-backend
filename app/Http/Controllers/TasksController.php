89a38181908d04650e7b06eaaa2ab4672df422d2
abukbt13 <abukbt13@gmail.com>
2023-05-31 17:55:22 +0300

    success edit

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -80,16 +80,11 @@ class TasksController extends Controller
         return response()->json($task);
     }
     public function updateone(Request $request, $id) {
-        $task = Task::where('id',$id)->get(); // Find the task with the given ID
-
-        $task->todo = $request['etodo']; // Update the 'etodo' field with the new value
-
+        var_dump($request->etodo);
+        $task = Task::find($id);
+        $task->todo = $request->etodo;
         $task->save();
-        return response()->json([
-            'message' =>"updated successfully",
-            'data' => $task
-        ]);
+        return response()->json($request->etodo);
     }
-
 
 }
